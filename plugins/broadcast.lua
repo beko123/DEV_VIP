481053c9e87881196644c01a804ee20d7827b4d2
beko123 <bekoiq@mailna.me>
2017-03-27 17:49:17 +0300

    Update broadcast.lua

--- plugins/broadcast.lua
+++ plugins/broadcast.lua
@@ -21,8 +21,8 @@ local function run(msg, matches)
 end
 return {
   patterns = {
-    "^[#!/](broadcast) (.*)$",
-    "^[#!/](bc) (%d+) (.*)$"
+    "^[#!/](نشر) (.*)$",
+    "^[#!/](بث) (%d+) (.*)$"
   },
   run = run
 }
